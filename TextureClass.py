09c3c3e8977a52ad9ffb6e605872b09f22e936a2
Shaun Kulesa <69469909+ShaunKulesa@users.noreply.github.com>
2021-06-05 13:39:39 +0100

    Update TextureClass.py

--- TextureClass.py
+++ TextureClass.py
@@ -1,1 +1,18 @@
+class Texture:
+    def __init__(self, path: str):
+        image = Image.open(path).convert("RGBA")
+        image = image.transpose(Image.FLIP_TOP_BOTTOM)
 
+        self.id = GL.glGenTextures(1)
+
+        GL.glBindTexture(GL.GL_TEXTURE_2D, self.id)
+        GL.glTexParameterf(GL.GL_TEXTURE_2D,GL.GL_TEXTURE_MIN_FILTER,GL.GL_NEAREST)
+        GL.glTexParameterf(GL.GL_TEXTURE_2D,GL.GL_TEXTURE_MAG_FILTER,GL.GL_NEAREST)
+        GL.glTexParameterf(GL.GL_TEXTURE_2D,GL.GL_TEXTURE_WRAP_S,GL.GL_CLAMP_TO_EDGE)
+        GL.glTexParameterf(GL.GL_TEXTURE_2D,GL.GL_TEXTURE_WRAP_T,GL.GL_CLAMP_TO_EDGE)
+        GL.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_BASE_LEVEL, 0)
+        GL.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MAX_LEVEL, 0)
+        GL.glTexImage2D(GL.GL_TEXTURE_2D, 0, GL.GL_RGBA, image.size[0], image.size[1], 0, GL.GL_RGBA, GL.GL_UNSIGNED_BYTE, image.tobytes())
+
+    def bind(self):
+        GL.glBindTexture(GL.GL_TEXTURE_2D, self.id)
